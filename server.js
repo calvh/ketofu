fdec7c61480879d5ae2bc9c967c90499ceffa290
calvh <calvinho.ca@gmail.com>
2019-04-16 13:02:24 -0400

    Removed unnecessary packages

--- server.js
+++ server.js
@@ -1,5 +1,3 @@
-require("dotenv").config();
-
 // -------------------------------  EXPRESS  ------------------------------
 const PORT = process.env.PORT || 3000;
 const express = require("express");
